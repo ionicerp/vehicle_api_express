0d1c8c24bd95ff52e7f9146c784a264f5eb3ec61
Vincent Tan <vincenttan83@live.com>
2023-07-02 21:21:13 +0800

    trying to force update

--- src/controllers.ts
+++ src/controllers.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 
 export const get = async (req: Request, res: Response) => {
     try {
-        res.status(200).json({ result: 'haha', keys: process.env.SECRET_KEY });
+        res.status(200).json({ result: 'haha', version: '1', keys: process.env.SECRET_KEY });
     } catch (error) {
         res.status(500).json({ error: (error as Error).message });
     }
