d3d9ff4dd983ca68725762b6d930fbbe2d58f391
Vincent Tan <vincenttan83@live.com>
2023-07-03 16:06:53 +0800

    force typed

--- src/authzero.ts
+++ src/authzero.ts
@@ -1,14 +1,30 @@
-const jwt = require('express-jwt');
-const jwksRsa = require('jwks-rsa');
 
-export const checkJwt = jwt({
-    secret: jwksRsa.expressJwtSecret({
+// import jwt from 'express-jwt';
+// import jwksRsa from 'jwks-rsa';
+
+// export const checkJwt = jwt.expressjwt({
+//   secret: jwksRsa.expressJwtSecret({
+//     cache: true,
+//     rateLimit: true,
+//     jwksRequestsPerMinute: 5,
+//     jwksUri: `https://ionicerp.us.auth0.com/.well-known/jwks.json`
+//   }),
+//   audience: 'tYk0jDwd29OG9V8BmZJl3FoXaRr5y8GO',
+//   issuer: `https://ionicerp.us.auth0.com/`,
+//   algorithms: ['RS256']
+// });
+
+import { expressjwt } from 'express-jwt';
+import { expressJwtSecret } from 'jwks-rsa';
+
+export const checkJwt = expressjwt({
+    secret: expressJwtSecret({
         cache: true,
         rateLimit: true,
         jwksRequestsPerMinute: 5,
-        jwksUri: `https://ionicerp.us.auth0.com/.well-known/jwks.json`
-    }),
+        jwksUri: `https://ionicerp.us.auth0.com/.well-known/jwks.json`,
+    }) as any,
     audience: 'tYk0jDwd29OG9V8BmZJl3FoXaRr5y8GO',
     issuer: `https://ionicerp.us.auth0.com/`,
-    algorithms: ['RS256']
+    algorithms: ['RS256'],
 });
