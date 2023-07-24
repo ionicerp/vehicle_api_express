ed64ef16ad99ddd4de87f8ca2178fd36a169e7ce
Vincent Tan <vincenttan83@live.com>
2023-07-24 20:23:58 +0800

    change of routing

--- src/routes.ts
+++ src/routes.ts
@@ -6,18 +6,41 @@ import { checkJwt } from './authzero';
 
 const router = express.Router();
 // router.get('/list', ctrl.get);
-router.post('/login', ctrl.postApiLogin)
-router.get('/private', checkJwt, ctrl.privateCheck);
-router.get('/environmentvalue', checkJwt, ctrl.getSampleEnvValue);
+// router.post('/login', ctrl.postApiLogin)
+// router.get('/private', checkJwt, ctrl.privateCheck);
+// router.get('/environmentvalue', checkJwt, ctrl.getSampleEnvValue);
 // router.get('/get', checkJwt, ctrl.getVehicle);
 // router.post('/create', checkJwt, ctrl.createVehicle);
 
-router.post('/create', checkJwt, vehicleController.createVehicle);
-router.get('/list', checkJwt, vehicleController.getAllVehicles);
-router.get('/get/:id', checkJwt, vehicleController.getSingleVehicle);
-router.put('/update/:id', checkJwt, vehicleController.updateVehicle);
-router.delete('/delete/:id', checkJwt, vehicleController.deleteVehicle);
-router.get('/search', checkJwt, vehicleController.searchVehicles);
+// router.post('/create', checkJwt, vehicleController.createVehicle);
+// router.get('/list', checkJwt, vehicleController.getAllVehicles);
+// router.get('/get/:id', checkJwt, vehicleController.getSingleVehicle);
+// router.put('/update/:id', checkJwt, vehicleController.updateVehicle);
+// router.delete('/delete/:id', checkJwt, vehicleController.deleteVehicle);
+// router.get('/search', checkJwt, vehicleController.searchVehicles);
+
+// Create a new vehicle
+router.post('/', checkJwt, vehicleController.createVehicle);
+
+// Get all vehicles or search vehicles based on query parameters
+router.get('/', checkJwt, vehicleController.getAllVehicles);
+
+// Get a single vehicle by ID
+router.get('/:id', checkJwt, vehicleController.getSingleVehicle);
+
+// Update a specific vehicle by ID
+router.put('/:id', checkJwt, vehicleController.updateVehicle);
+
+// Delete a specific vehicle by ID
+router.delete('/:id', checkJwt, vehicleController.deleteVehicle);
+
+// get    https://api.ionicerp.com/vehicle/v1/list
+// get    https://api.ionicerp.com/vehicle/v1/search
+
+// get    https://api.ionicerp.com/vehicle/v1/168
+// post   https://api.ionicerp.com/vehicle/v1/create
+// put    https://api.ionicerp.com/vehicle/v1/168
+// delete https://api.ionicerp.com/vehicle/v1/168
 
 // create a vehicle
 // get all vehicles
