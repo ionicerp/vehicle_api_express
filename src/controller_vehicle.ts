603172d3eb97296555013b7ce83ab0deedf524e6
Vincent Tan <vincenttan83@live.com>
2023-07-24 08:37:29 +0800

    output company ids

--- src/controller_vehicle.ts
+++ src/controller_vehicle.ts
@@ -17,7 +17,7 @@ const verifyToken = (token: string | undefined): DecodedToken => {
 
 const validateCompanyIds = (companyIds: string[], companyId: string): void => {
     if (!Array.isArray(companyIds)) {
-        throw new Error('companyIds should be an array.');
+        throw new Error('companyIds should be an array. Currently the value:' + companyIds);
     }
     if (!companyIds.includes(companyId)) {
         throw new Error('Illegal company id detected');
